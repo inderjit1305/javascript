d1b61678dd0c72eb0996c687950084a23abc67a9
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-21 20:26:15 +0530

    learnt Datatypes

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+/* Datatypes:
+
+ There are two datatypes:
+ 1.Primitive datatypes:
+ .Numver
+ .String
+ .Boolean
+ .Null
+.Undefined
+
+2.Non-primitive datatype:
+ .Object
+ .Array
+ .Function*/
+
+ let num = 50;
+ console.log(num);
+
+ let str = "inder";
+ console.log(str);
+
+ let bool = true;
+ console.log(bool);
+
+ let a = null;
+ console.log(a);
+
+ let und = b;
+ console.log(und);
