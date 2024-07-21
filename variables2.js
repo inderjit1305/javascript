d7ade703b8036adadfe975df5ce48431fa6ceb42
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-21 19:30:17 +0530

    var

---
+++ variables2.js
@@ -0,0 +1,16 @@
+let myName="Tajinder Singh"
+let a = myName;
+console.log(a);
+
+let Name = "Inderjit KAur"
+name = "Mehak";
+console.log(name);
+
+
+var Nickname = "Pratham";
+var Nickname= "Naryan";
+console.log(Nickname);
+
+const Town = "haler";
+const village = "Bohan";
+console.log(village);
