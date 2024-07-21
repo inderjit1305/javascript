b7b94f43b0077096b54f35fa6e898b1aa20d6e22
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-21 21:14:09 +0530

    Implicit type conversion

---
+++ typeconverstion.js
@@ -0,0 +1,13 @@
+result = "3"+2;
+console.log(result);
+console.log(typeof result);
+
+result = "3";
+result = Number(result);
+console.log(result);
+console.log(typeof result); 
+
+result = 20;
+result = parset(result);
+console.log(result);
+console.log(typeof result);
