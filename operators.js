46e15b4f7aea38a4ed6b64254ed8244c9ace6ed2
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-23 21:31:05 +0530

    Learnt Operators

---
+++ operators.js
@@ -0,0 +1,76 @@
+//Arithmetic Operators
+// Additional operator
+
+console.log(10+5);
+console.log(10-5);
+console.log(10/2);
+console.log(10*5);
+console.log(10%5);
+console.log(10**5);
+
+// Increment and Decrement 
+
+  let a = 1;
+// a++;                  Postfix  Pehle value print hogi fer increment hogi
+// ++a;                  Prefix   pehle  increment fer baad meh value print hogi
+console.log(a++);
+console.log(++a);
+
+// Comparsion Operator
+
+a = 5;
+b = 6;
+console.log(a==b);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+
+// Type Conversion
+
+c = "6";
+d = 6;
+console.log(c===d);
+console.log(c!==d);
+
+e = true;
+f = 1;
+console.log(e===f);
+console.log(e!==f);
+
+
+// Logical Operators
+
+// Logical OR
+
+// A  B  A||B
+// F  F   F
+// T  F   T
+// F  T   T
+// T  T   T
+
+console.log(4>3 || 2>3);
+
+// Logical AND
+
+// A B A&&B
+// F F  F
+// T F  F 
+// F T  F 
+// T T  T
+
+console.log(2>3 && 4>3);
+
+// Logical NOT A!
+
+
+// A  A!
+// F  T 
+// T  F 
+// F  T 
+// T  F
+
+console.log(!true);
+
+
