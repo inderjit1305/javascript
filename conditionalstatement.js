60398200ad40b0d07643a37728469ede8889843e
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-24 22:29:46 +0530

    learnt if else

---
+++ conditionalstatement.js
@@ -0,0 +1,18 @@
+// Condoitional
+
+let age = 19;
+let name = "Zayn";
+if(age>=18)
+{
+    if(name=="John")   //Nested
+    { 
+
+    console.log("You are eligible");
+}
+else{
+    console.log("Name is not in match");
+}
+    }
+else{
+    console.log("Yoy are not eligible");
+}
