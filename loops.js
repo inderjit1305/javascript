4ff51b979ca965f7c25ff11583fb9279587767c8
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-24 23:48:51 +0530

    Learn while loop

--- loops.js
+++ loops.js
@@ -11,3 +11,4 @@ for(var i=0;i<=10;i++)
    // console.log("2X"+i+"="+2*i);
    console.log(`2 X ${i} = ${2*i}`);
 }
+
---
+++ whileloop.js
@@ -0,0 +1,8 @@
+// while loop
+
+var i=1;
+while(i<=10)
+{
+    console.log(`2 X ${i} = ${2*i}`);
+     i++;
+}
