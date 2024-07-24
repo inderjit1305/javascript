bc996012e5d5b31b400059032a3b7c98c4485a94
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-24 22:48:44 +0530

    Learnt switch statement

---
+++ switch.js
@@ -0,0 +1,19 @@
+// Switch Statement
+
+let ch = 4;
+
+switch(ch)
+{
+  case 1:
+    console.log("I am 1");
+    break;
+    case 2:
+        console.log("I am 2");
+        break;
+        case 3:
+            console.log("I am 3");
+            break;
+            default:
+                console.log("You select a wrong choice");
+                break;
+}
