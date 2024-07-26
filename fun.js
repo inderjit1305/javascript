9eaa853e207b26fa171c4f0e53f0c680e15dd79b
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-26 22:44:48 +0530

    Learnt functions

---
+++ fun.js
@@ -0,0 +1,30 @@
+//  Functions
+
+function name(a)
+{
+    console.log(" Hello " +a);
+}
+name("Learners");
+
+function add(a,b)
+{
+    console.log(a+b);
+}
+add(2,3);
+
+function sub(a,b)
+{
+    console.log(a-b);
+}
+let x = 2;
+let y = 3;
+sub(x,y);
+
+function adds(a,b,c)
+{
+    console.log(a+b+c);
+}
+let f = 2;
+let g = 3;
+add(f,g,3);   // directly value lna
+
