6e94819b5527751b667d756da829e1bfd8b69e2e
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-27 15:33:24 +0530

    Learn concat,indexOf,reverse the arry

---
+++ array3.js
@@ -0,0 +1,9 @@
+let num = [10,20,30,40,50];
+let num2 = [5,10,15,20,25,30];
+
+// console.log(num.concat(num2));     Concat array ko jorne ka kam krta hai
+
+// console.log(num.indexOf(20));       indexof =  kon sa indes hai jeh btata hai 
+
+console.log(num.reverse());
+console.log(num2.sort());
