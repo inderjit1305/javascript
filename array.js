d4f6ec1794d3d5fee4a0552db5fcb9a1cabbe7f6
inderjit1305 <kaurinderjit18702@gmail.com>
2024-07-27 15:02:52 +0530

    Learnt Array

---
+++ array.js
@@ -0,0 +1,21 @@
+let name = ["Inder","Tajinder","Harshita"];
+console.log(name);
+
+
+let nam = ["Tajinder","Inder","Harleen"];
+console.log(nam[0]);
+
+let number = [1,2,3,4,5,6,7,8,9];
+console.log(number.length);
+
+let Nams = ["John",1,true,null];
+Nams.push("Cena");       // Psuh = sabse last meh add kar vane ke lyi use hota hai push
+console.log(Nams);
+
+let nams = ["John",1,true,null];
+nam.unshift("Cena");
+console.log(nam);       // Unshift = sabse agge add kar vane ke lyi use hota hai Unshift
+
+let Name = ["Johan",1,true,null];
+Name.splice(1,0,"Cena");      // Splice = center meh kar vane ke lyi splice ka use hota hai
+console.log(Name);
