81f0b51b5c580a94de72ac0003a1d64a72bd7cfe
inderjit1305 <kaurinderjit18702@gmail.com>
2024-08-10 17:03:02 +0530

    Learn for in

---
+++ arrforin.js
@@ -0,0 +1,11 @@
+//array
+
+let arr = [10,20,30,40,50];
+
+for(let ar in arr)
+{
+    // console.log(ar);
+    console.log(arr[ar]);  //value print kane ke lyi
+}
+
+
---
+++ arrforof.js
@@ -0,0 +1,6 @@
+let arr = [11,22,33,44,55];
+
+for(let ar of arr)   // for of meh values direct print ho jati hai
+{
+    console.log(ar);
+}
