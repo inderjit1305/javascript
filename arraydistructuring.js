ae3ba55ad19c49a5dd96254b9debe8c254902ceb
inderjit1305 <kaurinderjit18702@gmail.com>
2024-09-02 23:06:51 +0530

    Learnt array distructuring

---
+++ arraydistructuring.js
@@ -0,0 +1,15 @@
+let array1 = ["John","Chad"];
+let [item1,item2] = array1;
+
+console.log(item1);
+console.log(item2);
+
+// Spread method
+
+let array2 = ["Johem","Chad","Neo","Thomos"];
+let [index1,index2,...index3] = array2;
+
+console.log(index1);
+console.log(index2);
+console.log(index3);
+
