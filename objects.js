13d92c01db51f86614a237f9880653bedf241abe
inderjit1305 <kaurinderjit18702@gmail.com>
2024-09-04 22:56:07 +0530

    Learn objects

---
+++ objects.js
@@ -0,0 +1,25 @@
+let obj = {
+    name : "Bravo",
+    age : 21,
+    "last name" : "John"  // Double Codes
+}
+// console.log(obj.name)
+// console.log(obj.age);
+console.log(obj["last name"]);
+
+// Function
+
+let obj1 = {
+    fun : function()
+    {
+        console.log("I am function");
+    },
+    arr : [10,20,30,40]
+    
+}
+obj1.fun()
+console.log(obj1.arr);
+
+
+
+
