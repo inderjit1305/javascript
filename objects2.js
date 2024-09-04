4b772e1a5d5708e449cd259a48eff57bae86549f
inderjit1305 <kaurinderjit18702@gmail.com>
2024-09-04 23:09:14 +0530

    Learn objects

---
+++ objects2.js
@@ -0,0 +1,18 @@
+let obj = 
+{
+    "name" : "John",
+    "age" : 22,
+    "last name" : "Bravo",
+    fun : function()
+    {
+        console.log("I am function");
+    },
+    arr : [10, 20, 30]
+};
+
+// obj.fun();
+// console.log(obj.arr);
+let a = "key2"
+obj.key = "item";
+obj[a] = "item2"
+console.log(obj); 
